6ebf052a392e475a95acc3edf41c007dfc6d3045
Tarek Salah <tarek.salah.kamel@gmail.com>
2017-03-22 19:27:38 +0200

    Move project files into GitHub

---
+++ Assets/VRND-Course2-Project/Scripts/TriggerAnimation.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class TriggerAnimation : MonoBehaviour
+{
+	public string AnimationName;
+	public Animator stateMachine;
+
+	public Teleporter teleporter;
+	// Reference to the Teleporter script
+
+	//	private bool created = false;
+
+	void Awake ()
+	{
+//		if (GvrViewer.Instance == null) {
+//			GvrViewer.Create ();
+//			created = true;
+//		}
+	}
+
+	void Start ()
+	{
+//		if (created) {
+//			foreach (Camera c in GvrViewer.Instance.GetComponentsInChildren<Camera>()) {
+//				c.enabled = false; // to use the Gvr SDK without adding cameras we have to disable them
+//			}
+//		}
+
+	}
+
+	void Update ()
+	{
+
+		GvrViewer.Instance.UpdateState (); //need to update the data here otherwise we dont get mouse clicks; this is because we are automatically creating the GVRSDK (seems like a bug)
+
+		if (GvrViewer.Instance.Triggered) { // True if the Cardboard button is pressed .. However, we need to gurantee that it was pointing to the Globe.
+
+			RaycastHit hit;  // Will be used to check where the viewer was pointing at.
+
+			if (Physics.Raycast (transform.position, transform.forward, out hit)) { // Store information on the gameObject that was pointed at in "hit".
+//				Debug.Log ("Collision Detected");
+				if (hit.collider.gameObject.CompareTag ("Globe")) {  // If that gameObject has a tag equals to "Globe", then we should start rotation of the Globe.
+//					Debug.Log ("We hit the Globe");
+					teleporter.enabled = false;                      // First: Disable the Teleporter script to remain in front of the Globe and not quickly move to another waypoint.
+//					Debug.Log ("Teleporter disabled");
+					stateMachine.SetTrigger (AnimationName);         // Activate rotation  ... Will be stopped when the viewer presses the cardboard again when pointing to the Globe.
+//					Debug.Log ("The Globe is rotating");
+					teleporter.enabled = true;						 // Allow the viewer to move around when he/she desires. 
+//					Debug.Log ("Teleporter enabled");
+
+				}
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# VR-Udacity-Project2-Apartment
-This is project 2 submission in the VR Developer Nanodegree of Udacity. (Apartment)
+VR Nanodegree Course 2 Project
+-------------------------------------------------------------------------------
+
+Name: Tarek Salah Kamel
+Time: 16:00 hours
+1 thing I liked: Desiging the Appartment and Making the Globe Rotate
+1 thing was challenging: Adjusting the Lighting of the Scene
+
+Google VR SDK version: From this link https://github.com/googlevr/gvr-unity-sdk/releases/tag/v1.0.3  ... GVR Unity SDK v1.0.3
+
+LightMap Settings:
+ - The more I increased the baking resolution, I got brighter effects of light sources in the scene with clear sudden changes from less bright areas to more bright areas.
